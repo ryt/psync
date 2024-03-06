adc277b3b6d3f6d710d5121d546da73edd64b6c3
Ray <ray@redment.com>
2024-03-06 09:55:13 -0800

    upgrade to python3

--- psync.py
+++ psync.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 __author__  = 'Ray, github.com/ryt'
-__version__ = 'psync version 1.0.2'
+__version__ = 'psync version 1.1.2'
 __license__ = 'MIT'
 
 import sys
 from argparse import ArgumentParser
-from ConfigParser import ConfigParser
+from configparser import ConfigParser
 from collections import OrderedDict
 from subprocess import call
 
@@ -35,17 +35,17 @@ if args.conf:
 def main():
   if args.list:
     if any(apps):
-      print ''
+      print('')
       app_list()
-      print ''
+      print('')
     else:
-      print 'No apps yet, go add some!'
+      print('No apps yet, go add some!')
   elif args.app or args.aname:
     app_run(args)
   elif args.version:
-    print __version__
+    print(__version__)
   else:
-    print 'Specify an app name, or to get a list use -l or --list'
+    print('Specify an app name, or to get a list use -l or --list')
       
 def app_run(args):
   name = args.app or args.aname
@@ -59,25 +59,25 @@ def app_run(args):
     if args.d:
       cmd = ['rsync', opt, '--delete', eopt, dir1, dir2]
     if args.s or args.c:
-      print ' '.join(cmd)
+      print(' '.join(cmd))
       exit()
     call(cmd)
   else:
-    print "App (%s) dun't exist!" % name
+    print("App (%s) dun't exist!" % name)
       
       
 def app_list():
   len1, len2, len3 = [], [], []
-  for app, fol in apps.iteritems():
+  for app, fol in apps.items():
     len1.append(len(app))
     len2.append(len(fol[0]))
     len3.append(len(samef(fol)))
   max1, max2, max3 = max(len1), max(len2), max(len3)
-  for app, fol in apps.iteritems():
+  for app, fol in apps.items():
     sp1 = max1-len(app)
     sp2 = max2-len(fol[0])
     sp3 = max3-len(samef(fol))
-    print ' %s%s   %s%s   %s%s' % (app, spc(sp1), fol[0], spc(sp2), samef(fol), spc(sp3) )
+    print(' %s%s   %s%s   %s%s' % (app, spc(sp1), fol[0], spc(sp2), samef(fol), spc(sp3) ))
 
 def samef(l):
   return l[1].replace(':same', ':' + l[0]) 
@@ -86,7 +86,7 @@ def spc(num):
   return ''.join([' ']*num) if num else ''
 
 def err_nolist():
-  print "App list not found in '%s'. Add a valid list or specify config file (-f, --conf) " % (conf)
+  print("App list not found in '%s'. Add a valid list or specify config file (-f, --conf) " % (conf))
 
 
 # parse config file
